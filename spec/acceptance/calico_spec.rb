f22c4cc0194bf1def896eb3c3d84afc10944fa39
Alex Box <acbox@users.noreply.github.com>
2021-03-30 00:43:55 +0100

    Fix first Beaker test

--- spec/acceptance/calico_spec.rb
+++ spec/acceptance/calico_spec.rb
@@ -1,14 +1,23 @@
 require 'spec_helper_acceptance'
 
 describe 'calico install' do
-  let(:manifest) {
+  before(:all) do
+    manifest = <<-EOS
+    include docker
+    include etcd
+    EOS
+    apply_manifest(manifest, :catch_failures => true)
+  end
+
+  let(:manifest) do
     <<-EOS
     class { '::calico':
       # Calico CNI plugin binary patched for Nomad support
       cni_binary => 'https://github.com/acbox/cni-plugin/releases/download/v3.18.1-acbox-0/calico-amd64',
     }
     EOS
-  }
+  end
+
   it 'should apply without errors' do
     apply_manifest(manifest, :catch_failures => true)
   end
